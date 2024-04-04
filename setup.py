e55899600a5dd9ba3e927ba855fbd23a3dc0ede5
Alam <alamansari226570@gmail.com>
2024-04-04 11:28:01 +0530

    updated setup template

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "entbappy"
-SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_USER_NAME = "alamansari11"
+SRC_REPO = "Text-Summarizer"
+AUTHOR_EMAIL = "alamansari226570@gmail.com"
 
 
 
